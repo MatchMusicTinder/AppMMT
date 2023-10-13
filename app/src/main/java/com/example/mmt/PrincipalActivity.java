88f4452807e258451da52a6c81b6cdd1ee462c25
Seba Olima <edificarvcp@gmail.com>
2023-10-13 08:07:03 -0300

    dejo funcionando video

--- app/src/main/java/com/example/mmt/PrincipalActivity.java
+++ app/src/main/java/com/example/mmt/PrincipalActivity.java
@@ -6,72 +6,100 @@ import androidx.appcompat.widget.SearchView;
 
 import android.annotation.SuppressLint;
 import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-public class PrincipalActivity extends AppCompatActivity {
-//    SearchView buscar;
-//    private VideoView vdo;
+
+
+public class PrincipalActivity extends AppCompatActivity{
+    private static final String VIDEO_SAMPLE = "pantera";
+    private VideoView mVideoView;
+
+
+    private Uri getMedia(String mediaName) {
+        return Uri.parse("android.resource://" + getPackageName() +
+                "/raw/" + mediaName);
+    }
+    private int mCurrentPosition = 0;
+    private static final String PLAYBACK_TIME = "play_time";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        mVideoView = findViewById(R.id.videoview);
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+        MediaController controlador  = new MediaController(this) ;
+        controlador.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controlador);
 
-//        buscar = findViewById(R.id.buscar);
-//        buscar.setOnQueryTextListener(this);
-//
-//        int orientacion=getResources().getConfiguration().orientation;
-//        if (orientacion == Configuration.ORIENTATION_LANDSCAPE){
-//            getSupportActionBar().hide();
-//        }else
-//            getSupportActionBar().show();
-//
-//        vdo = findViewById(R.id.vdo);
-//
-//        vdo.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.pantera));
-//
-//        MediaController mdactrlr = new MediaController(this);
-//        vdo.setMediaController(mdactrlr);
-//        mdactrlr.setAnchorView(vdo);
-//
-//
-//        vdo.start();
-//    }
-//    @Override
-//    public boolean onQueryTextSubmit(String query) {
-//        return false;
-//    }
-//    @Override
-//    public boolean onQueryTextChange(String newText) {
-//        return false;
-//    }
-//     @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu,menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.action_profile1:
-//                Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.action_profile2:
-//                Toast.makeText(this, "Perfil Banda", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.action_profile3:
-//                Toast.makeText(this, "Compartir", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.action_profile4:
-//                Toast.makeText(this, "Ajustes", Toast.LENGTH_SHORT).show();
-//                break;
-//        }
-//        return super.onOptionsItemSelected(item);
+
     }
+
+    private void initializePlayer() {
+//
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
+//        if (mCurrentPosition > 0) {
+//            mVideoView.seekTo(mCurrentPosition);
+//        } else {
+//// Saltar a 1 muestra el primer cuadro del video.
+//            mVideoView.seekTo(1);
+//        }
+//
+       mVideoView.start();
+//        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            @Override
+//            public void onCompletion(MediaPlayer mediaPlayer) {
+//                Toast.makeText(PrincipalActivity.this, "Reproducción Completa",
+//                        Toast.LENGTH_SHORT).show();
+//                mVideoView.seekTo(1);
+//            }
+//        });
+//
+//
+   }
+   @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+    }
+
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializePlayer();
+   }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releasePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
 }
