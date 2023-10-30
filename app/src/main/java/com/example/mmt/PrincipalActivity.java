c073197b9a8030ce25a326bac9fe4014bbf3013a
Metalderly <osvaldoeverdiaz@gmail.com>
2023-10-30 12:23:00 -0300

    retrocediendo

--- app/src/main/java/com/example/mmt/PrincipalActivity.java
+++ app/src/main/java/com/example/mmt/PrincipalActivity.java
@@ -1,12 +1,12 @@
 package com.example.mmt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -98,5 +98,4 @@ public class PrincipalActivity extends AppCompatActivity{
         Intent intent = new Intent(this, PublicacionAudio.class);
         startActivity(intent);
     }
-
 }
